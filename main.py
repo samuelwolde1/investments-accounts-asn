4f7d596cc7fe94eac347ac18e0e3efdacbc48f71
samuelwolde1 <126281252+samuelwolde1@users.noreply.github.com>
2024-02-21 20:11:56 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ for account in range(1, 21):
     accounts.append([rand_num])
 
 while loop == True:
-    print('Main Menu')
+    print('\nMain Menu')
     print('\n1: Print Accounts')
     print('\n2: Deposit')
     print('\n3: Withdrawal')
@@ -18,12 +18,25 @@ while loop == True:
     selection = input('Enter Option #: ')
 
     if selection == '1':
+        print('\nACCOUNT BALANCES')
         for account in accounts:
-            print("Account " + str(accounts.index(account)) + ": $" + str(accounts[accounts.index(account)]))
+            print("Account " + str(accounts.index(account)) + ": $" + str(accounts[accounts.index(account)][0]))
     elif selection == '2':
-        print('Deposit')
+        print('\nDEPOSIT')
         account_selection = input('Enter the acount number: ')
         deposit_amount = input('Enter amount to deposit: $')
-        print('Account ' + str(accounts.index(account_selection)) + (' Previous Balance: ' + str(accounts[account_selection])))
-
-        
+        print('Account ' + str(account_selection) + ' Previous Balance: $'  + str(accounts[int(account_selection)][0]))
+        print('Account ' + str(account_selection) + ' New Balance: $'  + str(accounts[int(account_selection)][0] + int(deposit_amount)))
+    elif selection == '3':
+        print('\nWITHDRAWAL')
+        account_number = input('Enter account #: ')
+        withdrawal_amount = input('Enter amount to withdraw: $')
+        if accounts[int(account_number)][0] < int(withdrawal_amount):
+            print('Sorry, insufficient funds.')
+        else:
+            print('Account ' + str(account_number) + ' Previous Balance: $'  + str(accounts[int(account_number)][0]))
+            print('Account ' + str(account_number) + ' New Balance: $'  + str(accounts[int(account_number)][0] - int(withdrawal_amount)))
+    elif selection == '4':
+        for account in accounts:
+            if accounts[accounts.index(account)][0] < 2000:
+                print(account)
